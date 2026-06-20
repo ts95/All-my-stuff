3b582c752c3ebb65e3ed713367ccd12e89a52b48
Toni Sucic <toni.sucic@icloud.com>
2026-06-20 21:06:50 +0200

    Initial Commit

---
+++ All my stuff/All_my_stuffApp.swift
@@ -0,0 +1,17 @@
+//
+//  All_my_stuffApp.swift
+//  All my stuff
+//
+//  Created by Toni Sucic on 20/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct All_my_stuffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ All my stuffTests/All_my_stuffTests.swift
@@ -0,0 +1,19 @@
+//
+//  All_my_stuffTests.swift
+//  All my stuffTests
+//
+//  Created by Toni Sucic on 20/06/2026.
+//
+
+import Testing
+@testable import All_my_stuff
+
+struct All_my_stuffTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
